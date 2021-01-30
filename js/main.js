0cc7f08e0bdeb55a4c40edc145541d9d027755bc
MildaPau <76068034+MildaPau@users.noreply.github.com>
2021-01-30 12:33:02 +0200

    render socials function validation #16

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// ISKVIESTI FUNKCIJA
+import {renderSocials} from '../js/components/renderSocials.js';
+
+// PAIMTI DUOMENIS
+import {socialsData} from '../js/data/socialsData.js';
+
+// FUNKCIJAI PERDUOTI DUOMENIS
+renderSocials('footer > .socials', socialsData);
