33b99b9e549c61c5c89822e5d1ae968eb8fab3b2
MildaPau <76068034+MildaPau@users.noreply.github.com>
2021-01-30 15:44:35 +0200

    Progress-bar and CSS variable #18#19

---
+++ js/components/renderProgressBars.js
@@ -0,0 +1,32 @@
+function renderProgressBars(selector, data) {
+    //validation
+
+
+    //logic
+
+    const DOM = document.querySelector(selector);
+    let HTML = '';
+
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        
+        HTML += `<div class="progress-bar">
+                    <div class="top">
+                        <div class="label">${item.label}</div>
+                        <div class="value">${item.value}%</div>
+                    </div>
+                    <div class="bottom">
+                        <div class="progress" style="width: ${item.value}%"></div>
+                    </div>
+                </div>`;
+    }
+
+    //DOM.innerHTML += HTML;
+    DOM.insertAdjacentHTML('beforeend', HTML);    
+
+    //post logic validation
+    
+    //return rezult
+}
+
+export {renderProgressBars}
