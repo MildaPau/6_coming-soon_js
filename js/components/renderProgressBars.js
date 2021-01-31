c890b8f3af290ef5c946822a2eacd35b4dfc6f16
MildaPau <76068034+MildaPau@users.noreply.github.com>
2021-01-31 11:02:09 +0200

    Validation function for "render progress bars" #23

--- js/components/renderProgressBars.js
+++ js/components/renderProgressBars.js
@@ -1,4 +1,4 @@
-function renderProgressBars(selector, data) {
+/*function renderProgressBars(selector, data) {
     //validation
 
 
@@ -31,4 +31,83 @@ function renderProgressBars(selector, data) {
     //return rezult
 }
 
-export {renderProgressBars}
+export {renderProgressBars}  */
+
+
+import {formatNumber} from './formatNumber.js';
+
+function renderProgressBars(selector, data) {
+    //validation
+    if (typeof selector !== 'string' ||
+        selector === '' ) {
+        console.error('ERROR: netinkamo formato selector parametras.');
+        return false;
+    }
+    if (!Array.isArray(data) ||
+        data.length === 0 ){
+        console.error('ERROR: netinkamo formato data parametras');
+        return false;
+    }
+
+
+
+    //logic
+
+    const DOM = document.querySelector(selector);
+    if (!DOM) {
+        console.error('ERROR: pagal pateikta selectoriu elementas nerastas.');
+        return false;
+    }
+
+    let HTML = '';
+
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const formatedValue = formatNumber(item.value, 0);
+
+        // objekto validacija
+
+        if (typeof item !== 'object' ||
+            Array.isArray(item) ||
+            !item.label ||
+            typeof item.label !== 'string' ||
+            !item.value ||
+            typeof item.value !== 'number' ||
+            item.value < 0 ||
+            !isFinite(item.value) ||
+            typeof item.value !== 'number' ||
+            item.volue < 0 ||
+            item.value > 100 ||
+            formatedValue === false) {
+            
+            continue;
+        }
+        
+        HTML += `<div class="progress-bar">
+                    <div class="top">
+                        <div class="label">${item.label}</div>
+                        <div class="value">${formatedValue}%</div>
+                    </div>
+                    <div class="bottom">
+                        <div class="progress" style="width: ${item.value}%">
+                            <div class="loading"></div>
+                        </div>
+                    </div>
+                </div>`;
+    }
+
+      
+
+    //post logic validation
+    if (HTML === '') {
+        console.error('ERROR: duomenyse nera teisingu objektu, todel nesusigeneravo HTML');
+        return false;
+    }
+    
+    //return rezult
+    //DOM.innerHTML += HTML;
+
+    DOM.insertAdjacentHTML('beforeend', HTML);  
+}
+
+export { renderProgressBars }
